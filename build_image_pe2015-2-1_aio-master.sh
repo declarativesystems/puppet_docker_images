67de1fcecc5bafc86a2d860d980c6bcfec8620e8
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-09-29 00:15:47 +1000

    fix glytches

--- build_image_pe2015-2-1_aio-master.sh
+++ build_image_pe2015-2-1_aio-master.sh
@@ -28,14 +28,18 @@ docker run \
   --name $FINALNAME \
   $BASENAME
 
-# address of the docker-machine VM (boot2docker)
-DM_IP=$(docker-machine ip default)
+# address of the docker-machine VM (boot2docker) or just good 'ol localhost on
+# real computers
+DM_IP=$(docker-machine ip default 2>/dev/null|| echo localhost)
 
 # find the ssh port...
 SSH_PORT=$(docker inspect -f '{{ index .NetworkSettings.Ports "22/tcp" 0 "HostPort" }}' $FINALNAME)
 
+
 echo docker-machine IP $DM_IP, ssh port $SSH_PORT
-scp -p $SSH_PORT root@$DM_IP "./answers/all-in-one.answers.txt" "/root/all-in-one-answers.txt"
-ssh -p $SSH_PORT root@$DM_IP "cd /root/$PE_MEDIA && ./puppet-enterprise-installer -a /root/all-in-one.answers.txt"
+sleep 20
+scp -P $SSH_PORT ./answers/all-in-one.answers.txt "root@${DM_IP}:answers.txt"
+echo "answers file uploaded"
+ssh -p $SSH_PORT root@$DM_IP "cd /root/$PE_MEDIA && ./puppet-enterprise-installer -a /root/answers.txt"
 
 docker commit $FINALNAME $DOCKER_HUB_NAME
