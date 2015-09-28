9cda4bbc43231675caa1eda28f91e9d699eee612
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-09-28 23:30:03 +1000

    initial support for hostnames and random passwords

--- build_image_pe2015-2-1_aio-master.sh
+++ build_image_pe2015-2-1_aio-master.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+# Use user-supplied hostname if set
+if [ "$1" == "" ] ; then
+    export PUPPET_HOSTNAME="pe-puppet.localdomain"
+else
+    export PUPPET_HOSTNAME="$1"
+fi
+
+
 BASENAME="pe2015-2-1_centos-7"
 FINALNAME="${BASENAME}_aio-master"
-PUPPET_HOSTNAME="pe-puppet.localdomain"
 PE_MEDIA="puppet-enterprise-2015.2.1-el-7-x86_64"
 DOCKER_HUB_NAME="geoffwilliams/${BASENAME}_aio-master:v0"
 
@@ -26,7 +33,7 @@ DM_IP=$(docker-machine ip default)
 SSH_PORT=$(docker inspect -f '{{ index .NetworkSettings.Ports "22/tcp" 0 "HostPort" }}' $FINALNAME)
 
 echo docker-machine IP $DM_IP, ssh port $SSH_PORT
-
-ssh -p $SSH_PORT root@$DM_IP "cd /root/$PE_MEDIA && ./puppet-enterprise-installer -a answers/all-in-one.answers.txt"
+scp -p $SSH_PORT root@$DM_IP "./answers/all-in-one.answers.txt" "/root/all-in-one-answers.txt"
+ssh -p $SSH_PORT root@$DM_IP "cd /root/$PE_MEDIA && ./puppet-enterprise-installer -a /root/all-in-one.answers.txt"
 
 docker commit $FINALNAME $DOCKER_HUB_NAME
